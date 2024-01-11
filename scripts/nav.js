4d7daf945b6e6a0527573af8cd71fa3a88663b9a
sonia-mar <142726338+sonia-mar@users.noreply.github.com>
2024-01-11 17:25:29 +0000

    added to header - need to do js and need to do current page underline

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,10 @@
 $(window).on('resize', function() {
     // Narrowest screens:
-    if (window.matchMedia('(max-width: 910px)').matches) {
+    if (window.matchMedia('(max-width: 900px)').matches) {
         $('#hamburger-menu').removeClass('accessible-hidden');
         $('#nav-menu').addClass('accessible-hidden');
     }
-    if (window.matchMedia('(min-width: 910px)').matches) {
+    if (window.matchMedia('(min-width: 900px)').matches) {
         $('#hamburger-menu').addClass('accessible-hidden');
         $('#nav-menu').removeClass('accessible-hidden');
     }
@@ -21,7 +21,8 @@ $(document).ready(function() {
 $('#hamburger-menu').click(function() {
     if ($('#nav-menu').hasClass('accessible-hidden')) {
         $('#nav-menu').removeClass('accessible-hidden');
-    } else {
+    } 
+    else {
         $('#nav-menu').addClass('accessible-hidden');
     }
 })
