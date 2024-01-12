cccd70e1e5271cda471eecbc67ff00e750bd8bdc
sonia-mar <142726338+sonia-mar@users.noreply.github.com>
2024-01-12 03:52:34 +0000

    finished ??????

---
+++ scripts/iconlinks.js
@@ -0,0 +1,26 @@
+$("#medium").hover(function() {
+    $("#medium").addClass("accessible-hidden");
+    $("#mediumhover").removeClass("accessible-hidden");
+}, 
+  function() {
+    $("#mediumhover").addClass("accessible-hidden");
+    $("#medium").removeClass("accessible-hidden");
+  });
+
+$("#email").hover(function() {
+    $("#email").addClass("accessible-hidden");
+    $("#emailhover").removeClass("accessible-hidden");
+}, 
+  function() {
+    $("#emailhover").addClass("accessible-hidden");
+    $("#email").removeClass("accessible-hidden");
+  });
+
+$("#linkedin").hover(function() {
+    $("#linkedin").addClass("accessible-hidden");
+    $("#linkedinhover").removeClass("accessible-hidden");
+}, 
+  function() {
+    $("#linkedinhover").addClass("accessible-hidden");
+    $("#linkedin").removeClass("accessible-hidden");
+  });
